d4211f8e0d45adafdf0b695645b924d53c8f28c7
Samilly <samillynunes19@gmail.com>
2021-11-12 19:50:53 -0300

    Creating layers

---
+++ lib/application/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:sun_flare/presentation/home.dart';
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sun flare',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
